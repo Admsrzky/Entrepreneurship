368367763272ba96463363828c402d46c030072d
Admsrzky <adimasrizki926@gmail.com>
2024-03-18 18:45:21 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary:'#ffead0',
+        secondary:'#86888B',
+        dark: '#0f172a',
+        darkSecondary:'#7dd3fc'
+     }
+    },
+  },
+  plugins: [],
+}
+
